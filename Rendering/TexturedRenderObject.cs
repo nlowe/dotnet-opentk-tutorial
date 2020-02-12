f14cc9e6d84dca6c41beead872caef5df6c86d4c
Nathan Lowe <public@nlowe.me>
2020-02-11 21:59:27 -0500

    Fixes for 3.0+
    ImageSharp seems to be having trouble determining if Spans are available
    and is loading the wrong assembly at runtime. Upgrading to the latest
    version appears to fix this. This also changes a few things with how
    the app renders pixels.
    
    I think this means it also requires .NET Core 3.0+ but I need to do more
    digging.
    
    Closes #1

--- Rendering/TexturedRenderObject.cs
+++ Rendering/TexturedRenderObject.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using OpenTK.Graphics.OpenGL4;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace dotnet_opentk_tutorial.Rendering
 {
@@ -13,7 +15,7 @@ namespace dotnet_opentk_tutorial.Rendering
 
         private static int MIN_MIPMAPS = 0;
         private static int MIPMAP_LEVEL = 4;
-        
+
         public TexturedRenderObject(IReadOnlyCollection<TexturedVertex> vertices, ShaderProgram shaderProgram, string texturePath)
             : base(shaderProgram, vertices.Count)
         {
@@ -23,7 +25,7 @@ namespace dotnet_opentk_tutorial.Rendering
                 vertices.ToArray(),
                 BufferStorageFlags.MapWriteBit
             );
-            
+
             GL.VertexArrayAttribBinding(VAO, 0, 0);
             GL.EnableVertexArrayAttrib(VAO, 0);
             GL.VertexArrayAttribFormat(
@@ -34,7 +36,7 @@ namespace dotnet_opentk_tutorial.Rendering
                 false,
                 0
             );
-            
+
             GL.VertexArrayAttribBinding(VAO, 1, 0);
             GL.EnableVertexArrayAttrib(VAO, 1);
             GL.VertexArrayAttribFormat(
@@ -45,7 +47,7 @@ namespace dotnet_opentk_tutorial.Rendering
                 false,
                 sizeof(float) * 4
             );
-            
+
             GL.VertexArrayVertexBuffer(VAO, 0, VBO, IntPtr.Zero, TexturedVertex.Size);
 
             _textureId = InitTextures(texturePath);
@@ -54,7 +56,7 @@ namespace dotnet_opentk_tutorial.Rendering
         private static int InitTextures(string texturePath)
         {
             var data = LoadTexture(texturePath, out var w, out var h);
-            
+
             GL.CreateTextures(TextureTarget.Texture2D, 1, out int tid);
             GL.TextureStorage2D(
                 tid,
@@ -63,7 +65,7 @@ namespace dotnet_opentk_tutorial.Rendering
                 w,
                 h
             );
-            
+
             GL.BindTexture(TextureTarget.Texture2D, tid);
             GL.TextureSubImage2D(
                 tid,
@@ -76,7 +78,7 @@ namespace dotnet_opentk_tutorial.Rendering
                 PixelType.Float,
                 data
             );
-            
+
             GL.GenerateTextureMipmap(tid);
             GL.TextureParameterI(tid, All.TextureBaseLevel, ref MIN_MIPMAPS);
             GL.TextureParameterI(tid, All.TextureMaxLevel, ref MIPMAP_LEVEL);
@@ -86,7 +88,7 @@ namespace dotnet_opentk_tutorial.Rendering
 
             var textureMagFilter = (int) TextureMagFilter.Linear;
             GL.TextureParameterI(tid, All.TextureMagFilter, ref textureMagFilter);
-            
+
             return tid;
         }
 
@@ -100,9 +102,12 @@ namespace dotnet_opentk_tutorial.Rendering
 
                 var result = new float[4 * w * h];
                 var i = 0;
-                foreach (var pixel in image.SavePixelData())
+                foreach (var pixel in image.GetPixelSpan())
                 {
-                    result[i++] = pixel / 255f;
+                    result[i++] = pixel.R / 255f;
+                    result[i++] = pixel.G / 255f;
+                    result[i++] = pixel.B / 255f;
+                    result[i++] = pixel.A / 255f;
                 }
 
                 return result;
